827131b2ce465da4d0f2169fa5ec51a860d88951
Andrew Wagner <andrewwagner27@gmail.com>
2026-07-29 18:44:42 -0400

    fix App.jsx imports

--- tenant-portal/src/App.jsx
+++ tenant-portal/src/App.jsx
@@ -3,34 +3,36 @@ import { useEffect, useState } from "react";
 import { supabase } from "./supabase";
 
 // Landlord
-import LandlordLogin         from "./LandlordLogin";
-import LandlordDashboard     from "./LandlordDashboard";
-import LandlordTenants       from "./LandlordTenants";
-import LandlordTenantDetail  from "./LandlordTenantDetail";
-import LandlordProperties    from "./LandlordProperties";
-import LandlordMaintenance   from "./LandlordMaintenance";
-import LandlordMaintenanceDetail from "./LandlordMaintenanceDetail";
-import LandlordFinancials    from "./LandlordFinancials";
-import LandlordMessages      from "./LandlordMessages";
-import LandlordReports       from "./LandlordReports";
-import LandlordSettings      from "./LandlordSettings";
-import LandlordApplications  from "./LandlordApplications";
+import LandlordLogin          from "./LandlordLogin";
+import LandlordDashboard      from "./LandlordDashboard";
+import LandlordTenants        from "./LandlordTenants";
+import LandlordTenantDetail   from "./LandlordTenantDetail";
+import LandlordProperties     from "./LandlordProperties";
+import LandlordMaintenance    from "./LandlordMaintenance";
+import LandlordFinancials     from "./LandlordFinancials";
+import LandlordMessages       from "./LandlordMessages";
+import LandlordReports        from "./LandlordReports";
+import LandlordSettings       from "./LandlordSettings";
+import LandlordApplications   from "./LandlordApplications";
 import ProtectedLandlordRoute from "./ProtectedLandlordRoute";
 
 // Tenant
-import LoginScreen           from "./LoginScreen";
-import TenantOnboarding      from "./TenantOnboarding";
-import HomeDashboard         from "./HomeDashboard";
-import RentPaymentScreen     from "./RentPaymentScreen";
-import MaintenanceScreen     from "./MaintenanceScreen";
-import MaintenanceDetail     from "./MaintenanceDetail";
-import MessagingScreen       from "./MessagingScreen";
-import BulletinBoard         from "./BulletinBoard";
-import AccountScreen         from "./AccountScreen";
-import DocumentsScreen       from "./DocumentsScreen";
-import AuthCallback          from "./AuthCallback";
-import LandingPage           from "./LandingPage";
-import ForgotPassword        from "./ForgotPassword";
+import LoginScreen            from "./LoginScreen";
+import TenantOnboarding       from "./TenantOnboarding";
+import HomeDashboard          from "./HomeDashboard";
+import RentPaymentScreen      from "./RentPaymentScreen";
+import TenantMaintenanceList  from "./TenantMaintenanceList";
+import TenantMaintenanceDetail from "./TenantMaintenanceDetail";
+import MessagingScreen        from "./MessagingScreen";
+import BulletinBoard          from "./BulletinBoard";
+import AccountScreen          from "./AccountScreen";
+import DocumentsScreen        from "./DocumentsScreen";
+import AuthCallback           from "./AuthCallback";
+import LandingPage            from "./LandingPage";
+import ForgotPassword         from "./ForgotPassword";
+import ResetPassword          from "./ResetPassword";
+import VendorAccess           from "./VendorAccess";
+import VendorTicket           from "./VendorTicket";
 
 function ProtectedTenantRoute({ children }) {
   const [status, setStatus] = useState("loading");
@@ -55,35 +57,38 @@ export default function App() {
     <Router>
       <Routes>
         {/* Public */}
-        <Route path="/"               element={<LandingPage/>}/>
-        <Route path="/login"          element={<LoginScreen/>}/>
+        <Route path="/"                element={<LandingPage/>}/>
+        <Route path="/login"           element={<LoginScreen/>}/>
         <Route path="/forgot-password" element={<ForgotPassword/>}/>
-        <Route path="/auth/callback"  element={<AuthCallback/>}/>
-        <Route path="/onboarding"     element={<TenantOnboarding/>}/>
+        <Route path="/reset-password"  element={<ResetPassword/>}/>
+        <Route path="/auth/callback"   element={<AuthCallback/>}/>
+        <Route path="/onboarding"      element={<TenantOnboarding/>}/>
+        <Route path="/vendor/:token"   element={<VendorAccess/>}/>
+        <Route path="/vendor/ticket/:token" element={<VendorTicket/>}/>
 
         {/* Tenant */}
-        <Route path="/home"        element={<ProtectedTenantRoute><HomeDashboard/></ProtectedTenantRoute>}/>
-        <Route path="/pay"         element={<ProtectedTenantRoute><RentPaymentScreen/></ProtectedTenantRoute>}/>
-        <Route path="/maintenance" element={<ProtectedTenantRoute><MaintenanceScreen/></ProtectedTenantRoute>}/>
-        <Route path="/maintenance/:id" element={<ProtectedTenantRoute><MaintenanceDetail/></ProtectedTenantRoute>}/>
-        <Route path="/messages"    element={<ProtectedTenantRoute><MessagingScreen/></ProtectedTenantRoute>}/>
-        <Route path="/bulletin"    element={<ProtectedTenantRoute><BulletinBoard/></ProtectedTenantRoute>}/>
-        <Route path="/account"     element={<ProtectedTenantRoute><AccountScreen/></ProtectedTenantRoute>}/>
-        <Route path="/documents"   element={<ProtectedTenantRoute><DocumentsScreen/></ProtectedTenantRoute>}/>
+        <Route path="/home"              element={<ProtectedTenantRoute><HomeDashboard/></ProtectedTenantRoute>}/>
+        <Route path="/pay"               element={<ProtectedTenantRoute><RentPaymentScreen/></ProtectedTenantRoute>}/>
+        <Route path="/maintenance"       element={<ProtectedTenantRoute><TenantMaintenanceList/></ProtectedTenantRoute>}/>
+        <Route path="/maintenance/:id"   element={<ProtectedTenantRoute><TenantMaintenanceDetail/></ProtectedTenantRoute>}/>
+        <Route path="/messages"          element={<ProtectedTenantRoute><MessagingScreen/></ProtectedTenantRoute>}/>
+        <Route path="/bulletin"          element={<ProtectedTenantRoute><BulletinBoard/></ProtectedTenantRoute>}/>
+        <Route path="/account"           element={<ProtectedTenantRoute><AccountScreen/></ProtectedTenantRoute>}/>
+        <Route path="/documents"         element={<ProtectedTenantRoute><DocumentsScreen/></ProtectedTenantRoute>}/>
 
         {/* Landlord */}
-        <Route path="/landlord/login" element={<LandlordLogin/>}/>
-        <Route path="/landlord"               element={<ProtectedLandlordRoute><LandlordDashboard/></ProtectedLandlordRoute>}/>
-        <Route path="/landlord/properties"    element={<ProtectedLandlordRoute><LandlordProperties/></ProtectedLandlordRoute>}/>
-        <Route path="/landlord/tenants"       element={<ProtectedLandlordRoute><LandlordTenants/></ProtectedLandlordRoute>}/>
-        <Route path="/landlord/tenants/:id"   element={<ProtectedLandlordRoute><LandlordTenantDetail/></ProtectedLandlordRoute>}/>
-        <Route path="/landlord/applications"  element={<ProtectedLandlordRoute><LandlordApplications/></ProtectedLandlordRoute>}/>
-        <Route path="/landlord/maintenance"   element={<ProtectedLandlordRoute><LandlordMaintenance/></ProtectedLandlordRoute>}/>
-        <Route path="/landlord/maintenance/:id" element={<ProtectedLandlordRoute><LandlordMaintenanceDetail/></ProtectedLandlordRoute>}/>
-        <Route path="/landlord/financials"    element={<ProtectedLandlordRoute><LandlordFinancials/></ProtectedLandlordRoute>}/>
-        <Route path="/landlord/messages"      element={<ProtectedLandlordRoute><LandlordMessages/></ProtectedLandlordRoute>}/>
-        <Route path="/landlord/reports"       element={<ProtectedLandlordRoute><LandlordReports/></ProtectedLandlordRoute>}/>
-        <Route path="/landlord/settings"      element={<ProtectedLandlordRoute><LandlordSettings/></ProtectedLandlordRoute>}/>
+        <Route path="/landlord/login"            element={<LandlordLogin/>}/>
+        <Route path="/landlord"                  element={<ProtectedLandlordRoute><LandlordDashboard/></ProtectedLandlordRoute>}/>
+        <Route path="/landlord/properties"       element={<ProtectedLandlordRoute><LandlordProperties/></ProtectedLandlordRoute>}/>
+        <Route path="/landlord/tenants"          element={<ProtectedLandlordRoute><LandlordTenants/></ProtectedLandlordRoute>}/>
+        <Route path="/landlord/tenants/:id"      element={<ProtectedLandlordRoute><LandlordTenantDetail/></ProtectedLandlordRoute>}/>
+        <Route path="/landlord/applications"     element={<ProtectedLandlordRoute><LandlordApplications/></ProtectedLandlordRoute>}/>
+        <Route path="/landlord/maintenance"      element={<ProtectedLandlordRoute><LandlordMaintenance/></ProtectedLandlordRoute>}/>
+        <Route path="/landlord/maintenance/:id"  element={<ProtectedLandlordRoute><LandlordMaintenance/></ProtectedLandlordRoute>}/>
+        <Route path="/landlord/financials"       element={<ProtectedLandlordRoute><LandlordFinancials/></ProtectedLandlordRoute>}/>
+        <Route path="/landlord/messages"         element={<ProtectedLandlordRoute><LandlordMessages/></ProtectedLandlordRoute>}/>
+        <Route path="/landlord/reports"          element={<ProtectedLandlordRoute><LandlordReports/></ProtectedLandlordRoute>}/>
+        <Route path="/landlord/settings"         element={<ProtectedLandlordRoute><LandlordSettings/></ProtectedLandlordRoute>}/>
 
         <Route path="*" element={<Navigate to="/" replace/>}/>
       </Routes>
